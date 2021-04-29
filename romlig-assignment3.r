8c15de100caa9e89bf17579e3e5e72d4c1bcdb4c
Lene Omdal <lene.omdal@hotmail.com>
2021-04-29 13:00:35 +0200

    added imageplot for real

--- romlig-assignment3.r
+++ romlig-assignment3.r
@@ -1,7 +1,27 @@
+library(fields)
+library(MASS)
+library(ggplot2)
 
-# problem 1
+path = "C:\\Users\\Lene\\Documents\\Skole\\romlig\\TMA4250-Spatial-statistics"
+
+# Problem 1
 
 #read the data
 
-data = read.delim(url("https://www.math.ntnu.no/emner/TMA4250/2020v/Exercise3/complit.dat"), 
-                        header = FALSE, , sep =" ")
+data.complit = read.delim(url("https://www.math.ntnu.no/emner/TMA4250/2020v/Exercise3/complit.dat"), 
+                        header = FALSE , sep =" ")
+data.seismic = read.delim(url("https://www.math.ntnu.no/emner/TMA4250/2020v/Exercise3/seismic.dat"), 
+                          header = FALSE , sep ="\t", col.names = c("seismic"))
+
+image.plot()
+
+matrix.seismic = data.matrix(seismic.data)
+grid = expand.grid(x = 1:75, y = 1:75)
+df.obs = data.frame(grid, obs = data.seismic$seismic)
+df.obs
+
+ggplot(data = df.obs, aes(x = x, y = y, fill = obs)) + geom_tile() + scale_fill_distiller(palette = "Spectral")+ 
+  labs(fill = "Seismic value") + ggtitle("Image plot of seismic data") + theme_minimal() + xlim(c(0,75)) + ylim(c(0,75))
+ggsave("imageplot_seismic.pdf", width = 5, height = 4, path = path)
+
+
