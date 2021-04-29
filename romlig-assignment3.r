b90121f159a1088dab9093af19dd06cbeb6c1389
Lene Omdal <lene.omdal@hotmail.com>
2021-04-29 10:15:02 +0200

    created r file

---
+++ romlig-assignment3.r
@@ -0,0 +1,11 @@
+
+
+
+
+
+# problem 1
+
+#read the data
+
+data = read.delim(url("https://www.math.ntnu.no/emner/TMA4250/2020v/Exercise3/complit.dat"), 
+                        header = FALSE, , sep =" ")
